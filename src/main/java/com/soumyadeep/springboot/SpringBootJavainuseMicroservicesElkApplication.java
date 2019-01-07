4ac1d5344060e13e38eb53f9162b87905ec1829c
Soumyadeep Saha <SOUMYADEEP_SAHA@CRGL-THIRDPARTY.COM>
2019-01-07 18:20:54 +0530

    first commit

---
+++ src/main/java/com/soumyadeep/springboot/SpringBootJavainuseMicroservicesElkApplication.java
@@ -0,0 +1,14 @@
+package com.soumyadeep.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavainuseMicroservicesElkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavainuseMicroservicesElkApplication.class, args);
+	}
+
+}
+
